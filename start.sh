91ea09d345895bc56d3f4ae4ae6612d2bca9811e
Karloku Sang <karloku@gmail.com>
2015-08-25 21:50:47 +0800

    first commit

---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# config svr
+mongod  --configsvr                                 \
+        --dbpath  /data/configdb                    \
+        --logpath /data/configdb/log/configsvr.log  \
+        --port 27000 --fork
+
+mongod  --shardsvr                                  \
+        --storageEngine=wiredTiger                  \
+        --dbpath  /data/db1                         \
+        --logpath /data/db1/log/shard1.log          \
+        --port 29001 --fork
+
+mongod  --shardsvr                                  \
+        --storageEngine=wiredTiger                  \
+        --dbpath  /data/db2                         \
+        --logpath /data/db2/log/shard1.log          \
+        --port 29002 --fork
+
+mongos  --configdb localhost:27000                  \
+        --port 27017
